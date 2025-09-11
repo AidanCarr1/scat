93a9afa42972f7ca05dc8e2ec007811e0df85f28
AidanCarr1 <Aidan.Carr1@marist.edu>
2025-09-10 20:49:51 -0400

    dusting up some things

--- game-app/game.js
+++ game-app/game.js
@@ -6,9 +6,10 @@ function rollDie() {
 
 function getRandomListOrder(number) {
     lists = [1,2,3,4,5,6,7,8,9,10,11,12];
-    //randomize order
+    // randomize order
     for (var i = lists.length-1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
+        // swap
         [lists[i], lists[j]] = [lists[j], lists[i]];
     }
     return lists;
@@ -19,15 +20,3 @@ module.exports = {
     getRandomListOrder
 };
 
-// TEST
-
-// for (var i = 0; i < 10; i++){
-//     console.log(getRandomListOrder());
-// }
-
-// for (var i = 0; i < 10; i++){
-//     console.log(rollDie()+" "+rollDie()+" "+rollDie()+" "+
-//                 rollDie()+" "+rollDie()+" "+rollDie()+" "+
-//                 rollDie()+" "+rollDie()+" "+rollDie()+" "+
-//                 rollDie()+" "+rollDie()+" "+rollDie());
-// }
